403dc7e3a1141eb9ca6f911655f511c0c0a59575
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-10 20:38:20 +0100

    Use endpoint from other file

--- Server/server.go
+++ Server/server.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
+	"github.com/NotKatsu/ConsoleChat/endpoints"
 	"github.com/pterm/pterm"
 )
 
-type apiError struct {
-	ErrorCode uint16 `json:"error_code"`
-}
-
-func heartbeatSend(w http.ResponseWriter, r *http.Request) {
-	authorizationHeader := r.Header.Get("Authorization")
-
-	if authorizationHeader == "" {
-		err := apiError{
-			ErrorCode: 65535,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusUnauthorized)
-
-		jsonError, _ := json.Marshal(err)
-		_, writeError := w.Write(jsonError)
-
-		if writeError != nil {
-			pterm.Fatal.WithFatal(true).Println("Failed to write the data to the connection.")
-		}
-	} else {
-		
-	}
-}
-
 func main() {
-	http.HandleFunc("/api/heartbeat/send", heartbeatSend)
+	http.HandleFunc("/api/heartbeat/send", endpoints.HeartbeatSend)
 
 	err := http.ListenAndServe(":8080", nil)
 
