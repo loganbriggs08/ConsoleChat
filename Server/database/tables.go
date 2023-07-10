58a79c73d3cb72bfab094f61dddecdc8ee5167de
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-10 21:13:18 +0100

    Create tables.go to create tables that are needed in the db

---
+++ Server/database/tables.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Create() bool {
+	database, err := sql.Open("sqlite3", "database.db")
+
+	if err != nil {
+		return false
+	} else {
+		tablesToCreate := `
+		CREATE TABLE IF NOT EXISTS active_heartbeats(authorization VARCHAR(255), last_heartbeat VARCHAR(255));
+		`
+
+		_, err := database.Exec(tablesToCreate)
+
+		if err != nil {
+			return false
+		} else {
+			return true
+		}
+	}
+}
