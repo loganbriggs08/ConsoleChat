30ed6e7445210647c4010e57288a23e81d27edac
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-10 19:50:17 +0100

    Create error struct, heartbeat send function and main function to handle

--- ConsoleChat-Server/server.go
+++ ConsoleChat-Server/server.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/pterm/pterm"
 )
 
-func main() {
+type apiError struct {
+	ErrorCode uint16 `json:"error"`
+}
 
+func heartbeatSend(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "EndPoint Reached")
+}
+
+func main() {
+	http.HandleFunc("/api/heartbeat/send", heartbeatSend)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		pterm.Fatal.WithFatal(true).Println(err)
+	}
 }
