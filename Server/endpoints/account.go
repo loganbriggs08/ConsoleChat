2ad5a6a3c1f2e6d939ef458f7e753d438702fcd5
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-11 16:17:31 +0100

    Add data to database then return it

--- Server/endpoints/account.go
+++ Server/endpoints/account.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/NotKatsu/ConsoleChat/authentication"
+	"github.com/NotKatsu/ConsoleChat/database"
 )
 
 type accountCreated struct {
@@ -23,6 +24,29 @@ func AccountCreate(w http.ResponseWriter, r *http.Request) {
 
 	SnowFlake := rand.Int63n(max-min+1) + min
 	Authorization := authentication.Encode(strconv.Itoa(int(SnowFlake))) + "." + authentication.Encode(strconv.Itoa(int(authentication.Since_Epoch()))) + "." + authentication.Encode(authentication.String(25))
+
+	databaseResponse := database.AddHeartBeat(Authorization, uint64(SnowFlake))
+
+	if databaseResponse == true {
+		newUser := accountCreated{
+			SnowFlake:     uint64(SnowFlake),
+			Authorization: Authorization,
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		newUserJson, _ := json.Marshal(newUser)
+		w.Write(newUserJson)
+	} else {
+		apiError := apiError{
+			ErrorCode: http.StatusBadRequest,
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+
+		errorJson, _ := json.Marshal(apiError)
+		w.Write(errorJson)
+	}
 
 	newUser := accountCreated{
 		SnowFlake:     uint64(SnowFlake),
